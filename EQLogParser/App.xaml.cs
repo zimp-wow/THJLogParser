307914d2e62ddde2eed9107f588aafee8c8fc49d
Brandon DeVore <46546477+BND10706@users.noreply.github.com>
2025-05-20 21:00:52 -0400

    splash screen on load

--- EQLogParser/App.xaml.cs
+++ EQLogParser/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Threading.Tasks;
 
 namespace EQLogParser
 {
@@ -7,6 +8,27 @@ namespace EQLogParser
   /// </summary>
   public partial class App : Application
   {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+      var splash = new SplashScreen();
+      splash.Show();
+
+      Task.Run(() =>
+      {
+        // Simulate loading work (replace with real loading if needed)
+        System.Threading.Thread.Sleep(1200);
+      }).ContinueWith(_ =>
+      {
+        Dispatcher.Invoke(() =>
+        {
+          var mainWindow = new MainWindow();
+          mainWindow.Show();
+          splash.Close();
+        });
+      });
+    }
+
     private void CloseOverlay_MouseClick(object sender, RoutedEventArgs e) => OverlayUtil.ResetOverlay();
   }
 }
---
+++ EQLogParser/SplashScreen.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace EQLogParser
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
